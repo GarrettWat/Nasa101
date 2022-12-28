aca290d6281f2fd6cf5e756cf76bb73792ce712c
GarrettWaters <garrettwate0818@gmail.com>
2022-12-28 12:28:54 -0500

    'APOD'

--- src/comps/Home.jsx
+++ src/comps/Home.jsx
@@ -5,10 +5,12 @@ import React from 'react'
 function Home() {
   return (
 // Header text cetner of screen
-    <div className=" flex h-screen place-items-center">
-      <div className='mx-auto place-items-center'>
-        <h1 className='py-3.5 text-6xl'>All Things Nasa</h1>
-        <button className="btn btn-outline mx-auto">Start Here</button>
+    <div className="flex-col py-20 text-center">
+      <div className='mx-auto'>
+        <h1 className=' text-6xl'>All Things Nasa</h1>
+        {/* <button className=" btn btn-outline my-5">Start Here</button> */}
+        <h1 className='my-5'>Currently Developing: <strong>Earth Image Generator</strong></h1>
+        <iframe className='mx-auto' width="560" height="315" src="https://www.youtube.com/embed/21X5lGlDOfg" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
       </div>
       
     </div>
--- src/comps/Navbar.jsx
+++ src/comps/Navbar.jsx
@@ -4,7 +4,7 @@ export default function Navbar() {
   return (
     <div className="navbar bg-base-100">
       <div className="flex-1">
-        <a className="btn btn-ghost normal-case text-xl">Nasa101</a>
+        <a href='/' className="btn btn-ghost normal-case text-xl">Nasa101</a>
       </div>
       <div className="flex-none">
         <ul className="menu menu-horizontal px-1">
